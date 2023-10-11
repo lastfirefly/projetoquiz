4beb0c8e0258b2a3ed5f8eecfbd39860001de0d5
Beatriz Miranda <35241285+lastfirefly@users.noreply.github.com>
2023-10-11 17:01:50 -0300

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,13 @@
+from time import sleep as sl
+
+
+if __name__ == "__main__":
+    print("\nOlá!\nHoje vamos testar seu conhecimento básico sobre Astronomia com quatro perguntinhas!")
+    sl(2)
+    #pedir informações da pessoa
+    print("\nPara que possamos começar, vou te explicar as regras.\nI) Responda as perguntas apenas com o teclado numérico.\
+              \nII) Caso não queira mais fazer o questionário, responda com '00' quando a idade for perguntada!\n")
+    sl(1)
+
+
+import projetonovo as pn

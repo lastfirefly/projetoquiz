bf2925e3dbce4284df2461cde03243883b46e094
Beatriz Miranda <35241285+lastfirefly@users.noreply.github.com>
2023-10-23 14:51:38 -0300

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -2,7 +2,8 @@
 
 import pandas as pd
 import numpy as np
-import datetime as dt
+import datetime 
+from datetime import datetime as dt
 from time import sleep as sl
 
 #Contagem para definir quantas pessoas responderam o teste
@@ -24,73 +25,95 @@ respostas = {    "1": "Sim/Verdadeiro",
                 "3": "Não sei responder"
 }
 
+# Gêneros
+genero = { "1" : "Homem Cis",
+           "2" : "Mulher Cis",
+           "3" : "Homem Trans",
+           "4" : "Mulher Trans",
+           "5" : "Não Binário",
+        }
+
+
 # Dados dos testers
 
 tester = {
-     
+
 }
 
 #classes para organizar as partes do código:
 class Menu:
-        
-    # Definir as variáveis a serem utilizadas
-    # idade = int(input("Sua idade: "))
-    # genero = int(input("Com qual gênero você se identifica?\n[1] - Homem Cis\n[2] - Mulher Cis\n[3] - Homem Trans\n[4] - Mulher Trans\n[5] - Não-Binário\n"))
-    # contagem = 0
-
 
     # Para iniciar o questionário, será solicitado ao usuário que informe a sua idade e gênero.
     def __init__(self, idade, genero):
         self.idade = idade
         self.genero = genero
     
-    # Menu inicial
-    def start(self, idade, genero):
-    #apresentar o questioário
-        print("Olá!\nHoje vamos testar seu conhecimento básico sobre Astronomia com quatro perguntinhas!")
-        sl(2)
-    #pedir informações da pessoa
-        print("Para que possamos começar, vou te explicar as regras.\nI) Responda as perguntas apenas com o teclado numérico.\
-              \nII) Caso não queira mais fazer o questionário, responda com '00' quando a idade for perguntada!")
-        sl(1)
-    
-    # # Loop para percorrer pergutas e oferecer opções de respostas
-    # for pk, pv in questoes.items():
-    #     print(f'\n{pk}: {pv}\n')
-    #     for rk, rv in respostas.items():
-    #         print(f"{rk}: {rv}\n")
-    #     respostasUsuario = input("Sua resposta: ")
-    #     print("\n")
-    #     print("*"*50)
 
+    # Menu inicial
+
+ 
     while True:
 
-            dadosTester = [] 
+        dadosTester = [] 
+
+        idade = input("Sua idade: ")
+                
+        print("\n")
+
+        if idade == "00":
+            print ("\nObrigado por participar do teste!\n")
             
+            False
+            break
+        try:
+            idade = int(idade)
 
-            idade = int(input("Sua idade: "))
-            dadosTester.append(idade)
+            if idade < 1 or idade > 99:
+                print('\nIdade inválida. A idade deve estar entre 1 e 99.\n')
+                continue  # Volta ao início do loop para pedir a idade novamente
 
-            if idade == 00:
-                print ("\nObrigado por participar do teste!\n")
-                False
-                break
             else:
                 contagem += 1
-                genero = int(input("\nCom qual gênero você se identifica?\n[1] - Homem Cis\n[2] - Mulher Cis\n[3] - Homem Trans\n[4] - Mulher Trans\n[5] - Não-Binário\n"))
+
+                #Imprimir opções de gênero
+                
+                for gk, gv in genero.items():
+                    print(f'{gk}: {gv}')
+                genero = int(input("\nCom qual gênero você se identifica? \n"))
                 dadosTester.append(genero)
 
-                for pk, pv in questoes.items():
-                    print(f'\n{pk}: {pv}\n')
-                    for rk, rv in respostas.items():
-                        print(f"{rk}: {rv}\n")
-                    respostaUsuario = input("Sua resposta: ")
-                    dadosTester.append(respostaUsuario)
-                    print("\n")
-                    print("*"*50)
+                        # Anotar a hora do começo das respostas para que seja registrada
+                horaResposta = dt.now()
+                        # horaResposta.strftime('%d/%m/%Y')
+                dadosTester.append(horaResposta)
 
+
+                for pk, pv in questoes.items():
+                    while True:
+                        print(f'\n{pk}: {pv}\n')
+
+                        for rk, rv in respostas.items():
+                            print(f"{rk}: {rv}\n")
+
+                        respostaUsuario = input("Sua resposta: ")
+                        if not respostaUsuario in respostas:
+                            print("\n")
+                            print("*"*50)
+                            print(f'\nOpção inválida. Tente novamente.\n')
+                            print("*"*50)
+                        else:
+                            dadosTester.append(respostaUsuario)
+                            print("\n")
+                            print("*"*50)
+                            break
+                        
+        except ValueError:
+            print('\nResposta inválida! Digite apenas números inteiros.')
+
+            dadosTester.append(idade)
             print(dadosTester)
 
             # Atribuir os dados ao dicionário de dados dos testers
-            tester[contagem] = dadosTester
+        tester[contagem] = dadosTester
 
+        print(tester)
